30e674f6f674eb0d0cf6701f9bf853fd877c6ad3
Nana Faty <filbertnanablessing@gmail.com>
2025-06-25 11:31:07 +0000

    deploying my resume to elastic beanstalk

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve main HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
